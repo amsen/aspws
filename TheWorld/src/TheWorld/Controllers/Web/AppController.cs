50e05245051809076d4a7028fa9ccfc04f836254
Amit Sengupta <amit.ksengupta@gmail.com>
2017-05-05 18:34:49 +0530

    Adding the rest of the files as base project

---
+++ TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailservice, IConfigurationRoot config)
+        {
+            _mailService = mailservice;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("", "We Dont support aol.com");
+            }
+
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent!";
+            }
+
+            _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "Mail from TheWorld!", model.Message); 
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,37 @@
+﻿// site.js
+
+(function () {
+    //var ele = $("#username");
+    //ele.text = "Divya Sengupta";
+
+    //var main = $("#main");
+    //main.on("mouseenter",function () {
+    //    main.style.background = "#888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style.background = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var $sideMenuAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sideBarToggle").on("click",function(){
+        $sideMenuAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sideMenuAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        };
+
+    });
+
+
+})();
